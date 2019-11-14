11aa3de5c1c906df43efdf96bcc3af95621cb7e2
Thomas Bryson <tfbryson@yahoo.com>
2019-11-14 16:16:18 -0500

    pushing to repull

--- 12-Capstone/java/src/main/java/com/techelevator/npgeek/Model/Weather/Weather.java
+++ 12-Capstone/java/src/main/java/com/techelevator/npgeek/Model/Weather/Weather.java
@@ -54,28 +54,29 @@ public class Weather {
 	public void setForecast(String forecast) {
 		this.forecast = forecast;
 	}
-public Double getCelsiusLow() {
-		
-		DecimalFormat df = new DecimalFormat("#.##");      
-		Double results =(fahrenheitLow - 32.0) * (5.0 / 9.0);
-		results = Double.valueOf(df.format(results));
-		
-		return results;
-	} 
-
-	public void setCelsiusLow(Double celsiusLow) {
-		this.celsiusLow = celsiusLow;
-	} 
-
-	public Double getCelsiusHigh() {
-		DecimalFormat df = new DecimalFormat("#.##");      
-		Double result =(fahrenheitHigh - 32.0) * (5.0 / 9.0);
-		result = Double.valueOf(df.format(result));
-
-		return result;
-	}
-
-	public void setCelsiusHigh(Double celsiusHigh) {
-		this.celsiusHigh = celsiusHigh;
-	}
+	
+//public Double getCelsiusLow() {
+//		
+//		DecimalFormat df = new DecimalFormat("#.##");      
+//		Double results =(fahrenheitLow - 32.0) * (5.0 / 9.0);
+//		results = Double.valueOf(df.format(results));
+//		
+//		return results;
+//	} 
+//
+//	public void setCelsiusLow(Double celsiusLow) {
+//		this.celsiusLow = celsiusLow;
+//	} 
+//
+//	public Double getCelsiusHigh() {
+//		DecimalFormat df = new DecimalFormat("#.##");      
+//		Double result =(fahrenheitHigh - 32.0) * (5.0 / 9.0);
+//		result = Double.valueOf(df.format(result));
+//
+//		return result;
+//	}
+//
+//	public void setCelsiusHigh(Double celsiusHigh) {
+//		this.celsiusHigh = celsiusHigh;
+//	}
 }
